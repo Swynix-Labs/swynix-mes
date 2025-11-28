06d95a495bf66e3972891dad8e2c182fbd4a4acd
s-iliyas <shaik.m.iliyas@gmail.com>
2025-11-28 23:38:43 +0530

    remove child workspaces

--- swynix_mes/setup/install.py
+++ swynix_mes/setup/install.py
@@ -1,24 +1,10 @@
 import frappe
 
-WORKSPACES = (
-	"mes_overview",
-	"foundry_mes",
-	"caster_mes",
-	"crm_mes",
-	"foil_reel_mes",
-	"finishing_circle_mes",
-)
-
-
-def load_workspaces():
-	for workspace in WORKSPACES:
-		frappe.reload_doc("swynix_mes", "workspace", workspace, force=True)
-
 
 def after_install():
-	load_workspaces()
+	pass
 
 
 def after_migrate():
-	load_workspaces()
+	pass
 
