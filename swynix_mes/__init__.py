5c3518fd30589046cc3d998d2db3117a22b0f4a7
s-iliyas <shaik.m.iliyas@gmail.com>
2025-11-30 21:15:39 +0530

    fix

--- swynix_mes/__init__.py
+++ swynix_mes/__init__.py
@@ -1,1 +1,14 @@
 __version__ = "0.0.1"
+
+import frappe
+
+# Re-export API method at app root so calls to `swynix_mes.fetch_recipe_materials`
+# (used by old Server/Client Scripts) still work.
+from .api import fetch_recipe_materials as _fetch_recipe_materials_impl
+
+
+@frappe.whitelist()
+def fetch_recipe_materials(recipe_name):
+	"""Compatibility wrapper that forwards to `swynix_mes.api.fetch_recipe_materials`."""
+	return _fetch_recipe_materials_impl(recipe_name)
+
